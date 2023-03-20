5e40ba2f1a689ebb5eff008afa1a91cb754d3c47
Enrique Munoz <enriquemunozdev@gmail.com>
2023-03-19 20:31:55 -0600

    login and logout ready

--- application.py
+++ application.py
@@ -78,21 +78,21 @@ def login():
             return "bad ps" #apology("must provide password", 403)
 
         # Query database for username
-        query = text("SELECT * FROM users WHERE username = :username")
-        rows = db.execute(query, {"username":request.form["username"]}).fetchall()
+        query = text("SELECT user_id, username, hashed_pass FROM users WHERE username = :username")
+        user = db.execute(query, {"username":request.form["username"]}).fetchone()
         #db.commit()
         
-        # print(rows[0])
+        # print(rows)
+        # for flight in rows:
+        #      print(f"{flight.user_id} to {flight.username}, {flight.hashed_pass} minutes.")
         # print(type(rows[0]))
-            
-        #could use fetchone to only access a list but, even like that i have to access the cols with int indexes and i want to use them with str
 
         # Ensure username exists and password is correct 
-        if len(rows) != 1 or not check_password_hash(rows[0][2], request.form["password"]): #row 0 col 2
+        if user == None or not check_password_hash(user.hashed_pass, request.form["password"]): #row 0 col 2
             return "bad login" #apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
-        session["user_id"] = rows[0][0]
+        session["user_id"] = user.user_id
 
         # Redirect user to home page
         return redirect(url_for("index"))
@@ -100,6 +100,16 @@ def login():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
+    
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
 
 
 @app.route("/")
